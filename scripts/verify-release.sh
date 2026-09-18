95b18baf8b1ff884ba104784937bf82b753ac12c
Andrew Chalkley <andrew@chalkley.org>
2026-09-17 22:51:24 -0700

    test(storage): run cross-board SD and ESP filesystem gates in CI

--- scripts/verify-release.sh
+++ scripts/verify-release.sh
@@ -282,6 +282,10 @@ check_runtime_registry
 check_release_artifacts
 check_platform_boundaries
 bash "${ROOT_DIR}/scripts/test-storage-fatfs.sh"
+sh "${ROOT_DIR}/tests/run-sd-spi-tests.sh"
+bash "${ROOT_DIR}/tests/run-sticky-sd-tests.sh"
+bash "${ROOT_DIR}/tests/run-esp-volume-tests.sh"
+bash "${ROOT_DIR}/tests/run-esp-sd-tests.sh"
 pass 'real FatFs USB-volume copy and preservation checks passed'
 check_docs_build
 
